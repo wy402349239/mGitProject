b9e0903fae68c42a245d3d200fd813bfacdb6ec7
zz_wangyu <WangYu789>
2018-10-05 15:04:21 +0800

    添加bitmap

---
+++ app/src/main/java/com/project/git/com/gitproject/bitmap/BitmapActivity.java
@@ -0,0 +1,17 @@
+package com.project.git.com.gitproject.bitmap;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018\10\5 0005.
+ */
+
+public class BitmapActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
