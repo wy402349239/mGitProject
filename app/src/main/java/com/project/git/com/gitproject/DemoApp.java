ab4ad8f5bd21e6407fa215fdb174fe4284913796
wy_40234 <402349239@qq.com>
2019-02-27 18:14:57 +0800

    8.0API  添加shortcut，长按桌面图标菜单

--- app/src/main/java/com/project/git/com/gitproject/DemoApp.java
+++ app/src/main/java/com/project/git/com/gitproject/DemoApp.java
@@ -1,10 +1,23 @@
 package com.project.git.com.gitproject;
 
+import android.annotation.TargetApi;
 import android.app.Application;
+import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.net.Uri;
+import android.os.Build;
 
+import com.project.git.com.gitproject.levitate.FloatActivity;
+import com.project.git.com.gitproject.pic.PicScrollActivity;
 import com.tencent.bugly.Bugly;
 import com.tencent.tinker.loader.app.TinkerApplication;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -50,6 +63,40 @@ public class DemoApp extends TinkerApplication {
             }).subscribeOn(Schedulers.newThread());
         }
         Bugly.init(DemoApp.this.getApplicationContext(), "578d30338d", true);
+        addCut();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N_MR1)
+    private void addCut(){
+        Intent intent = new Intent();//目标跳转intent
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.setClass(DemoApp.this, PicScrollActivity.class);
+        ShortcutManager manager = getSystemService(ShortcutManager.class);
+        Intent intentMain = new Intent();//back stack intent
+        intentMain.setAction(Intent.ACTION_MAIN);
+        intentMain.setClass(DemoApp.this, MainActivity.class);
+        Intent[] intents = new Intent[]{intentMain, intent};//放置顺序：先放返回的，再放目标跳转的
+        ShortcutInfo infor = new ShortcutInfo.Builder(DemoApp.this, "shortId1")
+                .setShortLabel("label")
+                .setLongLabel("longlabel")
+                .setIcon(Icon.createWithResource(DemoApp.this, R.drawable.ic_fav_login_close))
+                .setIntents(intents)
+                .build();
+
+        Intent intent2 = new Intent();//目标跳转intent
+        intent2.setAction(Intent.ACTION_MAIN);
+        intent2.setClass(DemoApp.this, FloatActivity.class);
+        Intent[] intents2 = new Intent[]{intentMain, intent2};//放置顺序：先放返回的，再放目标跳转的
+        ShortcutInfo infor2 = new ShortcutInfo.Builder(DemoApp.this, "shortId2")
+                .setShortLabel("label2")
+                .setLongLabel("longlabel2")
+                .setIcon(Icon.createWithResource(DemoApp.this, R.drawable.home_change))
+                .setIntents(intents2)
+                .build();
+        List<ShortcutInfo> infos = new ArrayList<>();
+        infos.add(infor);
+        infos.add(infor2);
+        manager.setDynamicShortcuts(infos);
     }
 
     public static DemoApp getInstance() {
